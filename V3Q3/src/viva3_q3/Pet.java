5f732ac8a6cf7d4da6b85682025002990a6010bc
Kan Hoi Yeng <237968832+hyjanesa88@users.noreply.github.com>
2026-01-03 16:21:58 +0800

    Added Pet Class

---
+++ V3Q3/src/viva3_q3/Pet.java
@@ -0,0 +1,61 @@
+package viva3_q3;
+
+public class Pet {
+
+    private String name;
+    private String species;
+    private String breed;
+    private int age;
+    private String healthRecord;
+    private boolean isAdopted;
+    private String adopterName;
+
+    //Constructor (What should this object look like immediately after creation?)
+    public Pet (String name, String species, String breed, int age, String healthRecord) {
+        this.name = name;
+        this.species = species;
+        this.breed = breed;
+        this.age = age;
+        this.healthRecord = healthRecord;
+        this.isAdopted = false;
+        this.adopterName = null;
+    }
+    //to adopt a pet
+    public void adopt(String adopterName){
+        this.isAdopted = true;
+        this.adopterName = adopterName;
+    }
+
+    //to get pet details
+    public String getDetails(){
+        return "Name: " + name +
+                ", Species: " + species +
+                ", Breed: " + breed +
+                ", Age: " + age +
+                ", Health Record: " + healthRecord +
+                ", Adpoted: " + isAdopted +
+                ", Adopter Name: " + adopterName
+    }
+    //Getters
+    public String getName(){
+        return name;
+    }
+    public String getSpecies(){
+        return species;
+    }
+    public String getBreed(){
+        return breed;
+    }
+    public int getAge(){
+        return age;
+    }
+    public String getHealthRecord(){
+        return healthRecord;
+    }
+    public boolean isAdopted(){
+        return isAdopted;
+    }
+    public String getAdopterName(){
+        return adopterName;
+    }
+}
